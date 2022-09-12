eb4e68ed1a166a226a37542988513be00c2de3ca
MikeWight <michel.wight@hotmail.com>
2022-09-11 22:20:25 -0300

    just a littel styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
+
   return (
     <div>
-      <h1>Bookkeeper!</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
+      <Nav
+         defaultActiveKey="invoices"
       >
-        <Link to="/invoices">Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link> |{" "}
-        <Link to="/posts">Posts</Link> |{" "}
-        <Link to="/user">User</Link>
-      </nav>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand to="/">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link eventKey="invoices"  as={Link} to="/invoices">
+                  Invoices
+                </Nav.Link>
+                <Nav.Link eventKey="expenses"  as={Link} to="/expenses">
+                  Expenses
+                </Nav.Link>
+                <Nav.Link eventKey="posts"  as={Link} to="/posts">
+                  Posts
+                </Nav.Link>
+                <Nav.Link eventKey="user"  as={Link} to="/user">
+                  User
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Nav>
       <Outlet />
     </div>
   );
